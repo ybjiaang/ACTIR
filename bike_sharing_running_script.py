5418df52a6a2bfa2995e3b8022c61a6e751dfcad
Yibo Jiang <yiboj@Yibos-MacBook-Pro.local>
2021-12-07 13:01:30 -0600

    run 50 biking tests

--- bike_sharing_running_script.py
+++ bike_sharing_running_script.py
@@ -15,7 +15,7 @@ log_directory = "./log_base_year_1"
 if not os.path.exists(log_directory):
     os.makedirs(log_directory)
 
-bike_year = 1
+bike_year = 0
 for bike_season in range(4):
     filename = log_directory + "/" + "test_season_" + str(bike_season) + "_year_" + str(bike_year) + ".csv"
     if os.path.exists(filename):
@@ -25,6 +25,6 @@ for bike_season in range(4):
       writer = csv.writer(file)
       writer.writerow(["HSIC", "IRM", "ERM", "MAML", "Anti-Causal", "Causal"])
     
-    for _ in range(10):
+    for _ in range(50):
         cmd = 'python main.py --compare_all_invariant_models --dataset=bike --bike_test_season={:} --bike_year=0 --cvs_dir={:}'.format(bike_season, filename)
         run_cmd(cmd)
