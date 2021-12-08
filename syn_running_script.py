752846745602225d78152845b5ea47eac660ce8f
Yibo Jiang <yiboj@Yibos-MacBook-Pro.local>
2021-12-07 19:49:20 -0600

    change scripting file

--- syn_running_script.py
+++ syn_running_script.py
@@ -11,7 +11,7 @@ def run_cmd(cmd):
 	logging.info("Running command: {:}".format(cmd))
 	subprocess.check_call(cmd,shell=True)
 
-log_directory = "./log_syn_anti"
+log_directory = "./log_syn_causal"
 if not os.path.exists(log_directory):
     os.makedirs(log_directory)
 
@@ -22,8 +22,8 @@ if os.path.exists(filename):
 
 with open(filename, 'a', newline='') as file: 
     writer = csv.writer(file)
-    writer.writerow(["HSIC", "IRM", "ERM", "MAML", "Anti-Causal", "Causal"])
+    writer.writerow(["HSIC", "IRM", "ERM", "MAML", "Anti-Causal", "Causal Base", "Causal"])
 
-for _ in range(100):
-    cmd = 'python main.py --compare_all_invariant_models --causal_dir_syn=anti --cvs_dir={:}'.format(filename)
+for _ in range(10):
+    cmd = 'python main.py --compare_all_invariant_models --causal_dir_syn=causal --cvs_dir={:}'.format(filename)
     run_cmd(cmd)
