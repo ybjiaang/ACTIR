b9aa05fa28b4f1ff4f7172630cb51350fb9a3dc8
Yibo Jiang <yiboj@Yibos-MacBook-Pro.local>
2022-01-06 13:04:18 -0600

    bike hyperameter testing

--- trainers/adap_invar_trainer.py
+++ trainers/adap_invar_trainer.py
@@ -129,10 +129,6 @@ class AdaptiveInvariantNNTrainer():
         self.outer_optimizer.zero_grad()
         phi_loss.backward()
         self.outer_optimizer.step()
-
-      # with torch.no_grad():
-      #   for env_ind in range(n_train_envs):
-      #     self.model.etas[env_ind].data = self.model.etas[env_ind] / torch.norm(self.model.etas[env_ind]) * self.num_class
       
       if t % 10 == 0 and self.config.verbose:
         print(phi_loss.item()/(n_train_envs*batch_size))
