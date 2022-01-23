de7557fb4cef43428382038534fb9030897cf51f
Yibo Jiang <yiboj@Yibos-MacBook-Pro.local>
2022-01-23 13:41:10 -0600

    bike sharing test

--- dataset/bike_env.py
+++ dataset/bike_env.py
@@ -23,7 +23,7 @@ class BikeSharingDataset(object):
         self.input_dim = self.test_data[0].shape[1]
 
     def read_files(self):
-        data= pd.read_csv(self.cvs_dir, usecols = ['season', 'yr', 'holiday', 'weekday', 'workingday', 'hum', 'cnt'])
+        data= pd.read_csv(self.cvs_dir, usecols = ['season', 'yr', 'holiday', 'weekday', 'workingday', 'temp', 'casual'])
         data_array = data.to_numpy()
         self.train_data_by_season = []
         self.val_data_by_season = []
@@ -34,14 +34,14 @@ class BikeSharingDataset(object):
             total_num = season_data.shape[0]
             season_data_permutated = torch.Tensor(season_data[np.random.permutation(total_num)])
             season_data_permutated[:, -1] /= 100
-            if i == self.test_season:
+            if i == 2:
                 # season_data_permutated[:, -2] = 0
                 self.test_data_finetune = (season_data_permutated[:self.test_finetune_size, :-1], season_data_permutated[:self.test_finetune_size, [-1]])
                 self.test_data_unlabled = (season_data_permutated[self.test_finetune_size: self.test_finetune_size + self.test_unlabled_size, :-1], 
                     season_data_permutated[self.test_finetune_size: self.test_finetune_size + self.test_unlabled_size, [-1]])
                 self.test_data = (season_data_permutated[self.test_finetune_size + self.test_unlabled_size:, :-1], 
                     season_data_permutated[self.test_finetune_size + self.test_unlabled_size:, [-1]])
-            elif i == (self.test_season + 1) % self.num_total_envs:
+            elif i == 0: #self.num_total_envs:
                 # season_data_permutated[:, -2] = 0
                 val_x = season_data_permutated[:, :-1]
                 val_y = season_data_permutated[:, [-1]]
