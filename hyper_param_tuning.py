1c036d333b87393223b128b54ba87449fb511598
Yibo Jiang <yiboj@Yibos-MacBook-Pro.local>
2022-05-05 19:39:54 -0500

    update hyper parameter search space

--- hyper_param_tuning.py
+++ hyper_param_tuning.py
@@ -43,14 +43,18 @@ if __name__ == '__main__':
       os.remove(filename)
 
   if not args.reg_lambda_list:
-    if args.dataset == "color_mnist" or args.dataset == "anti":
+    if args.dataset == "color_mnist":
       args.reg_lambda_list = np.logspace(-1, 3, num=10)
+    elif args.dataset == "anti":
+      args.reg_lambda_list = np.logspace(-1, 2, num=20)
     else:
       args.reg_lambda_list = [10, 1, 0.1]
 
   if not args.reg_lambda_2_list:
-    if args.dataset == "color_mnist" or args.dataset == "anti":
+    if args.dataset == "color_mnist":
       args.reg_lambda_2_list = np.logspace(-1, 3, num=10)
+    elif args.dataset == "anti":
+      args.reg_lambda_2_list = np.logspace(-1, 2, num=20)
     else:
       args.reg_lambda_2_list = [0.1, 1, 5, 10, 0.01]
 
